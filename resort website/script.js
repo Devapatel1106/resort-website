3093ac55d4d2a517512d56cfc1aded6c0b216297
Devapatel1106 <devanshi.axisipl@gmail.com>
2025-10-03 13:47:58 +0530

    Initial commit - Resort Website

---
+++ resort website/script.js
@@ -0,0 +1,11 @@
+function msg(){
+ alert("Alertbox");
+}
+function msg2(){
+    var txt;
+    if (confirm("Press a button!")) {
+      txt = "You pressed OK!";
+    } else {
+      txt = "You pressed Cancel!";
+    }
+}
